4120cfd693d7dc10f55aa14b6b23e98e8297704d
Lenin Nava <lenin.an.ln@gmail.com>
2021-03-17 16:53:25 -0400

    Removed dist folder from gitignore

---
+++ dist/index.091f88ac.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("#app").style.display="block";const e=document.getElementsByClassName("icon");console.log(e);const t=document.getElementsByClassName("navButton");console.log(t),t[0].addEventListener("click",(()=>{e[0].classList.toggle("hidden"),e[1].classList.toggle("hidden")}))}();
+//# sourceMappingURL=index.091f88ac.js.map
