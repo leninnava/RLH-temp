93cd544acf902ec85e259f2f3ffaf19caa378d00
Lenin Nava <lenin.an.ln@gmail.com>
2021-03-17 22:31:08 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./styles.css";
 import "./styles.scss";
 console.log("hello world!");
+
+const navButton = document.querySelector("#navButton");
+const navIcons = document.querySelectorAll("#menuIcon");
+
+const navToggle = () => {
+  navIcons[0].classList.toggle("hidden");
+  navIcons[1].classList.toggle("hidden");
+  console.log("fired");
+};
+
+navButton.addEventListener("click", navToggle);
